6af2b28e69acceacf95a19b09521618bd13313e2
sdu20 <sdu2014@126.com>
2020-06-14 17:27:26 +0800

    run somedata

--- Run/run0610.py
+++ Run/run0610.py
@@ -12,7 +12,7 @@ def run_example():
     一个使用 local PCA 降维方法的示例
     :return:
     """
-    path = "E:\\ChinaGraph\\Data\\coil4class\\"
+    path = "E:\\ChinaGraph\\Data\\dolarsign5000\\"
     X = np.loadtxt(path + "data.csv", dtype=np.float, delimiter=",")
     label = np.loadtxt(path + "label.csv", dtype=np.int, delimiter=",")
     (n, m) = X.shape
@@ -25,13 +25,13 @@ def run_example():
         plt.show()
 
     params = {}
-    params['neighborhood_type'] = 'knn'  # 'knn' or 'rnn' or 'iter'
+    params['neighborhood_type'] = 'rnn'  # 'knn' or 'rnn' or 'iter'
     params['n_neighbors'] = 15  # Only used when neighborhood_type is 'knn'
-    params['neighborhood_size'] = 1.0  # Only used when neighborhood_type is 'rnn'
-    params['alpha'] = 0.8  # the weight of euclidean distance
-    params['beta'] = 1 - params['alpha']  # the weight of local PCA
+    params['neighborhood_size'] = 0.2  # Only used when neighborhood_type is 'rnn'
+    params['alpha'] = 0.91  # the weight of euclidean distance
+    params['beta'] = 1.0 - params['alpha']  # the weight of local PCA
     params['distance_type'] = 'spectralNorm'  # 'spectralNorm' or 'mahalanobis'
-    params['manifold_dimension'] = 1  # the real dimension of manifolds
+    params['manifold_dimension'] = 2  # the real dimension of manifolds
     params['perplexity'] = 30.0  # perplexity in t-SNE
     params['MAX_Distance_iter'] = 10  # max iter of distance computing
     params['use_skeleton'] = False  # boolean value. Whether use skeleton method.
