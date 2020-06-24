0966e1209bc777551234aee1073c364c336d518b
sdubrz <sdu2014@126.com>
2020-06-24 21:41:38 +0800

    中心化处理MNIST数据

--- MakeData/mnist.py
+++ MakeData/mnist.py
@@ -25,8 +25,84 @@ def test():
     print(X)
 
 
+def combination01():
+    path = "E:\\ChinaGraph\\DataLab\\MNIST68\\"
+    data0 = np.loadtxt(path+"6.csv", dtype=np.int, delimiter=",")
+    data1 = np.loadtxt(path+"8.csv", dtype=np.int, delimiter=",")
+    (n0, m) = data0.shape
+    (n1, m) = data1.shape
+    label0 = np.zeros((n0, 1))
+    label1 = np.ones((n1, 1))
+
+    X0 = data0 - np.mean(data0, axis=0)
+    X1 = data1 - np.mean(data1, axis=0)
+    np.savetxt(path+"data0.csv", X0, fmt='%d', delimiter=",")
+    np.savetxt(path+"data1.csv", X1, fmt='%d', delimiter=",")
+    np.savetxt(path+"label0.csv", label0, fmt='%d', delimiter=",")
+    np.savetxt(path+"label1.csv", label1, fmt='%d', delimiter=",")
+
+
+def big_combination():
+    # 将所有的 10 个类分别中心化，并全部组合起来
+    data_list = []
+    label_list = []
+    count = np.zeros((10, 1))
+    path = "E:\\ChinaGraph\\DataLab\\MNISTtest\\classify\\"
+    X = np.zeros((10000, 784))
+    label = np.zeros((10000, 1))
+    for i in range(0, 10):
+        data_i = np.loadtxt(path+"data"+str(i)+".csv", dtype=np.int, delimiter=",")
+        label_i = np.loadtxt(path+"label"+str(i)+".csv", dtype=np.int, delimiter=",")
+        (n_i, m_i) = data_i.shape
+        count[i] = n_i
+        if i == 0:
+            X[0:n_i, :] = data_i - np.mean(data_i, axis=0)
+            label[0:n_i] = 0
+        else:
+            head = int(np.sum(count[0:i]))
+            print("head = ", head)
+            X[head:head+n_i, :] = data_i - np.mean(data_i, axis=0)
+            label[head:head+n_i] = i
+        print(i)
+
+    np.savetxt(path+"data.csv", X, fmt='%d', delimiter=",")
+    np.savetxt(path+"label.csv", label, fmt='%d', delimiter=",")
+
+
+def classify():
+    """
+    按照类别划分
+    :return:
+    """
+    path = "E:\\ChinaGraph\\DataLab\\MNISTtest\\"
+    data0 = np.loadtxt(path + "data.csv", dtype=np.int, delimiter=",")
+    label0 = np.loadtxt(path+"label.csv", dtype=np.int, delimiter=",")
+    (n, m) = data0.shape
+    print(data0.shape)
+
+    n_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(0, n):
+        n_list[label0[i]] = n_list[label0[i]] + 1
+
+    for c in range(0, 10):
+        n1 = n_list[c]
+        label = c * np.ones((n1, 1))
+        data = np.zeros((n1, m))
+        count = 0
+        for i in range(0, n):
+            if label0[i] == c:
+                data[count, :] = data0[i, :]
+                count += 1
+        np.savetxt(path+"classify\\label"+str(c)+".csv", label, fmt='%d', delimiter=",")
+        np.savetxt(path+"classify\\data"+str(c)+".csv", data, fmt='%d', delimiter=",")
+        print(c)
+
+
 if __name__ == '__main__':
     # test()
-    run()
+    # run()
+    # combination01()
+    # classify()
+    big_combination()
 
 
--- Run/run0610.py
+++ Run/run0610.py
@@ -14,14 +14,13 @@ def run_example():
     :return:
     """
     path = "E:\\ChinaGraph\\Data\\"
-    data_name = "ParkinsonHW"
+    data_name = "MNISTtestCenter"
     path = path + data_name + "\\"
     X0 = np.loadtxt(path + "data.csv", dtype=np.float, delimiter=",")
     label = np.loadtxt(path + "label.csv", dtype=np.int, delimiter=",")
     (n, m) = X0.shape
 
     normalize = True
-    if normalize:
 
     # 如果是三维的，则画出三维散点图
     if m == 3:
@@ -39,7 +38,7 @@ def run_example():
 
     params = {}
     params['neighborhood_type'] = 'knn'  # 'knn' or 'rnn' or 'iter'
-    params['n_neighbors'] = 10  # Only used when neighborhood_type is 'knn'
+    params['n_neighbors'] = 20  # Only used when neighborhood_type is 'knn'
     params['neighborhood_size'] = 0.2  # Only used when neighborhood_type is 'rnn'
     params['alpha'] = 0.9  # the weight of euclidean distance
     params['beta'] = 1.0 - params['alpha']  # the weight of local PCA
@@ -49,9 +48,9 @@ def run_example():
     params['MAX_Distance_iter'] = 10  # max iter of distance computing
     params['use_skeleton'] = False  # boolean value. Whether use skeleton method.
 
-    affinity = 'PCA'  # affinity 的取值可以为 'cov'  'expCov'  'Q'  'expQ'  'MDS'  't-SNE'  'PCA'  'Isomap'  'LLE'
+    affinity = 'Q'  # affinity 的取值可以为 'cov'  'expCov'  'Q'  'expQ'  'MDS'  't-SNE'  'PCA'  'Isomap'  'LLE'
     # 'geo-t-SNE'
-    frame_work = 't-SNE'  # frame 的取值可以为 'MDS'  't-SNE'  't-SNE+'
+    frame_work = 't-SNE+'  # frame 的取值可以为 'MDS'  't-SNE'  't-SNE+'
     dr = LocalPCADR(n_components=2, affinity=affinity, parameters=params, frame=frame_work)
 
     Y = dr.fit_transform(X)
@@ -107,8 +106,8 @@ def run_example():
         multi_class_plt.multi_class_scatter(Y, label, run_str)
 
     # 画图片散点图
-    # from Tools import ImageScatter
-    # ImageScatter.mnist_scatter(data_name, run_str) 
+    from Tools import ImageScatter
+    ImageScatter.mnist_scatter(data_name, run_str)
 
 
 if __name__ == '__main__':
